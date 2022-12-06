5a6d1f168f0fa5443b7dd521d516619d9ad1c43d
Natalia <nivanova86@bk.ru>
2022-12-06 03:10:59 +0300

    add GetSelectArray method

---
+++ program/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] GetSelectArray(string[] array, int lengthConstraint)
+{
+    int resultArraySize = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i].Length <= lengthConstraint)
+            resultArraySize++;
+
+    string[] resultArray = new string[resultArraySize];
+    int k = 0;
+    for(int j = 0; j < resultArraySize; j++)
+        while(k < array.Length)
+        { 
+            if(array[k].Length <= lengthConstraint)
+            {
+                resultArray[j] = array[k];
+                k++;
+                break;
+            }
+            k++;
+        }
+
+    return resultArray;
+}
