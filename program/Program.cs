f0a6bf323600a0b2ac5890939a0f80ac1855a560
Natalia <nivanova86@bk.ru>
2022-12-06 03:15:53 +0300

    add program

--- program/Program.cs
+++ program/Program.cs
@@ -21,3 +21,27 @@
 
     return resultArray;
 }
+
+Console.Write("Input the size of the array of strings: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[size];
+Console.WriteLine("Input array elements:");
+for(int i = 0; i < size; i++)
+    array[i] = Console.ReadLine();
+Console.WriteLine();
+
+int lengthConstraint = 3;
+string[] selectArray = GetSelectArray(array, lengthConstraint);
+
+Console.WriteLine("your array of strings:");
+for(int i = 0; i < array.Length; i++)
+    Console.Write(array[i] + "  ");
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine($"array of strings less than or equal to {lengthConstraint} characters long:");
+for(int i = 0; i < selectArray.Length; i++)
+    Console.Write(selectArray[i] + "  ");
+Console.WriteLine();
+Console.WriteLine();
+
